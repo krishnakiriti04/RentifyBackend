1884e675b77b15343387903b5797671e09f62fd6
krishna kireeti <krishnakiriti.mamidi@gmail.com>
2020-12-02 00:34:30 +0530

    v3.1.0

--- routes/contactus.js
+++ routes/contactus.js
@@ -4,7 +4,8 @@ const nodemailer = require('nodemailer');
 
 const client = mongodb.MongoClient;
 
-const dburl = process.env.DB_URL || "mongodb://localhost:27017";
+//const dburl = process.env.DB_URL ||
+const dburl = "mongodb://localhost:27017";
 
 const express = require('express');
 let router = express.Router();
